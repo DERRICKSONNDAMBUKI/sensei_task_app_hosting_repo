d879a8579396058ab7ca509468940911d5b598f0
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-28 23:57:25 +0300

    hosting commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sensei_task_app_hosting_repo
+
+### Hosted
+[Sensei Task App](https://sensei-tasks-app.herokuapp.com/)
--- database/db.js
+++ database/db.js
@@ -1,10 +1,23 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-  user: "zsvnjlabxzfxuz",
-  password: "31735e27702e8c04064aa29ed019dca39cba30d6d67f0f65174e166554786d3e",
-  host: "ec2-34-231-63-30.compute-1.amazonaws.com",
-  port: 5432,
-  database: "deeodf263s05np",
+// const pool = new Pool({
+//   user: "zsvnjlabxzfxuz",
+//   password: "31735e27702e8c04064aa29ed019dca39cba30d6d67f0f65174e166554786d3e",
+//   host: "ec2-34-231-63-30.compute-1.amazonaws.com",
+//   port: 5432,
+//   database: "deeodf263s05np",
+// });
+// module.exports = pool;
+
+const { Client } = require('pg');
+
+const pool = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-module.exports = pool;
+
+pool.connect();
+
+module.exports=pool
